4646b91a3c2801547a0fe00b6199325da98bb751
Tanmay Bhole <95764001+tanmaybhole18@users.noreply.github.com>
2025-02-18 17:30:43 +0530

    Update App.jsx

--- IMDB_top_250_movies/src/App.jsx
+++ IMDB_top_250_movies/src/App.jsx
@@ -3,7 +3,8 @@ import Movie from "./components/Movie";
 import "./App.css";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState(""); 
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json")
@@ -11,11 +12,32 @@ const App = () => {
       .then((data) => setMovies(data));
   }, []);
 
+ 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value.toLowerCase()); 
+  };
+
+ 
+  const filteredMovies = movies.filter((movie) =>
+    movie.name.toLowerCase().includes(searchQuery) 
+  );
+
   return (
     <div>
       <h1>IMDb Top 250 Movies</h1>
+
+      <div className="input">  
+      <input 
+        className="input-box"
+        type="text"
+        placeholder="Search Movies..."
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      </div>
+      
       <div className="movie-grid">
-        {movies.map((movie, index) => (
+        {filteredMovies.map((movie, index) => (
           <Movie key={index} movie={movie} />
         ))}
       </div>
@@ -24,3 +46,8 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
